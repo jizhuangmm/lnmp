2dca59b226650408959f95671f397c8cc9bcee80
yangbiao <yangbiao@readtv.cn>
2017-06-14 09:50:49 +0800

    yaf.sh

--- php/extension/yaf.sh
+++ php/extension/yaf.sh
@@ -3,44 +3,44 @@
 case $php_version in
      5.3.29)
         mkdir -p $install_dir/server/php/lib/php/extensions/no-debug-non-zts-20090626
-        if [ ! -f ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz ];then
+        if [ ! -f yaf-3.0.4.tgz ];then
              wget http://pecl.php.net/get/yaf-3.0.4.tgz
         fi
         tar zxvf yaf-3.0.4.tgz
-        cd yaf-3.0.4.tgz
+        cd yaf-3.0.4
         $install_dir/server/php/bin/phpize
         ./configure --with-php-config=$install_dir/server/php/bin/php-config
         make
         make install
         cd ..
-        echo "extension=redis.so" >> $install_dir/server/php/etc/php.ini
+        echo "extension=yaf.so" >> $install_dir/server/php/etc/php.ini
         ;;
      5.6.30)
         mkdir -p $install_dir/server/php/lib/php/extensions/no-debug-non-zts-20090626
-        if [ ! -f ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz ];then
+        if [ ! -f yaf-3.0.4.tgz ];then
              wget http://pecl.php.net/get/yaf-3.0.4.tgz
         fi
         tar zxvf yaf-3.0.4.tgz
-        cd yaf-3.0.4.tgz
+        cd yaf-3.0.4
         $install_dir/server/php/bin/phpize
         ./configure --with-php-config=$install_dir/server/php/bin/php-config
         make
         make install
         cd ..
-        echo "extension=redis.so" >> $install_dir/server/php/etc/php.ini
+        echo "extension=yaf.so" >> $install_dir/server/php/etc/php.ini
         ;;
      7.1.3)
         mkdir -p $install_dir/server/php/lib/php/extensions/no-debug-non-zts-20090626
-        if [ ! -f ZendGuardLoader-php-5.3-linux-glibc23-x86_64.tar.gz ];then
+        if [ ! -f yaf-3.0.4.tgz ];then
              wget http://pecl.php.net/get/yaf-3.0.4.tgz
         fi
         tar zxvf yaf-3.0.4.tgz
-        cd yaf-3.0.4.tgz
+        cd yaf-3.0.4
         $install_dir/server/php/bin/phpize
         ./configure --with-php-config=$install_dir/server/php/bin/php-config
         make
         make install
         cd ..
-        echo "extension=redis.so" >> $install_dir/server/php/etc/php.ini
+        echo "extension=yaf.so" >> $install_dir/server/php/etc/php.ini
         ;;
 esac
